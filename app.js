cc047d71b4a2f29e277ccaa1f0b6177e7a1b6456
1352noComment <raisa.kabir@u.nus.edu>
2017-06-16 12:00:01 +0600

    Update app.js

--- app.js
+++ app.js
@@ -11,41 +11,40 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/loginsystem');
-mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/loginapp');
 var db = mongoose.connection;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-//Init System
+// Init App
 var app = express();
 
-//View Engine
+// View Engine
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
 app.set('view engine', 'handlebars');
 
-//BodyParser Middleware
+// BodyParser Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-//Set static Folder
+// Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Passport init
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport init
 app.use(passport.initialize());
 app.use(passport.session());
 
-//Express Session
-app.use(session({
-	secret: 'secret',
-	saveUninitialized: true,
-	resave: true
-}));
-
-//Express Validator
+// Express Validator
 app.use(expressValidator({
   errorFormatter: function(param, msg, value) {
       var namespace = param.split('.')
@@ -63,24 +62,26 @@ app.use(expressValidator({
   }
 }));
 
-//Connect Flash
+// Connect Flash
 app.use(flash());
 
-//Global Vars
+// Global Vars
 app.use(function (req, res, next) {
-	res.locals.success_msg = req.flash('success_msg');
-	res.locals.error_msg = req.flash('error_msg');
-	res.locals.error = req.flash('error');
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
   res.locals.user = req.user || null;
-	next();
+  next();
 });
+
+
 
 app.use('/', routes);
 app.use('/users', users);
 
-//Set Port
+// Set Port
 app.set('port', (process.env.PORT || 3000));
 
-app.listen(app.get('port'), function() {
-	console.log('Server startes on port' + app.get('port'));
+app.listen(app.get('port'), function(){
+  console.log('Server started on port '+app.get('port'));
 });
